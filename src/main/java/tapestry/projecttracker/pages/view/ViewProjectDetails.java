47ce3b3ba874ea78f936f81626ae5fbefca89bb8
dejan-ivanovic-2568 <divanovic@deform.rs>
2015-12-04 18:18:54 +0100

    project-details page update

--- src/main/java/tapestry/projecttracker/pages/view/ViewProjectDetails.java
+++ src/main/java/tapestry/projecttracker/pages/view/ViewProjectDetails.java
@@ -7,10 +7,14 @@ package tapestry.projecttracker.pages.view;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import tapestry.projecttracker.data.ProjectDAO;
+import tapestry.projecttracker.entities.Member;
 import tapestry.projecttracker.entities.Project;
+import tapestry.projecttracker.pages.edit.EditProject;
 
 /**
  *
@@ -23,9 +27,15 @@ public class ViewProjectDetails {
 
     @Property
     private Project project;
+    
+    @SessionState
+    private Member loggedInMember;
 
     @Property
     private List<Project> projects;
+
+    @InjectPage
+    private EditProject editProjectPage;
 
     public void set(Project project) {
         this.project = project;
@@ -46,11 +56,20 @@ public class ViewProjectDetails {
         return project;
     }
 
+    Object onEditProject() {
+        editProjectPage.set(this.project);
+        return editProjectPage;
+
+    }
+
     void onValidateFromProjectSelectForm() {
         System.out.println("VALIDATION CALLED.... " + ": PROJECT : " + project);
         if (project == null) {
             project = projects.get(0);
         }
     }
-    
+
+    public boolean getLoggedInRole() {
+        return (loggedInMember.getMemberRole().name() != "Administrator") ? true : false;
+    }
 }
